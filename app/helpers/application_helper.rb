ba41a41b258bfdcc1bda3f1f84f1bd3f69c92f18
HungVu0112 <pcsolaggy123@gmail.com>
2025-04-14 14:32:17 +0700

    Chapter 3,4,5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t "title.base_title"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
+
+  def tutorial_link
+    link_to t("pages.home.links.tutorial"), Settings.defaults.tutorial_url
+  end
+
+  def help_link
+    link_to t("pages.help.links.help"), Settings.defaults.help_url
+  end
+
+  def book_link
+    link_to t("pages.help.links.book_html"), Settings.defaults.book_url
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,9 @@ module RailsTutorial
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Config I18n
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :vi
   end
 end
