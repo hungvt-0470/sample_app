da461ce8dc4fd1424861573ef582fff6c83ec133
hungvt-0470 <vu.thai.hung@sun-asterisk.com>
2025-04-17 09:42:24 +0700

    Chapter_7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
+  USER_PARAMS = [
+    :name,
+    :email,
+    :password,
+    :password_confirmation
+  ].freeze
+
   before_save :downcase_email
 
   validates :name, presence: true,
             length: {maximum: Settings.users.name_max_length}
   validates :email, presence: true,
             length: {maximum: Settings.users.email_max_length},
-            format: {with: Regexp.new(Settings.users.email_validate_regex)},
+            format: {with: Regexp.new(Settings.users.email_validate_regex,
+                                      Regexp::IGNORECASE)},
             uniqueness: true
 
   has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
+
     get "static_pages/home"
     get "static_pages/help"
+
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: [:show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
