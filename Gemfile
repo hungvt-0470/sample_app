85e2858d5eaf8cb29e46111e2ada6f0d49124007
hungvt-0470 <vu.thai.hung@sun-asterisk.com>
2025-04-18 09:22:51 +0700

    Chapter_8

--- Gemfile
+++ Gemfile
@@ -49,3 +49,6 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+# Use Redis for Action Cable
+gem "redis", "~> 4.0"
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  LOGIN_PARAMS = [:email, :password].freeze
+
+  def new; end
+
+  def create
+    user = User.find_by email: login_params[:email]&.downcase
+
+    if user&.authenticate login_params[:password]
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = t "pages.login.form.invalid"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_path
+  end
+
+  private
+  def login_params
+    params.require(:session).permit(LOGIN_PARAMS)
+  end
+end
